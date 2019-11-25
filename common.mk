33971f273e446b65d131e7b0eb42b86571043e9f
Dyneteve <sound0020@gmail.com>
2019-11-25 19:35:24 +0200

    sm8150-common: Uprev mapper and allocator to 3.0
    Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>

--- common.mk
+++ common.mk
@@ -219,7 +219,7 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
-    android.hardware.graphics.mapper@2.0-impl-qti-display \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     gralloc.msmnile \
@@ -235,7 +235,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.allocator@1.0-service \
     vendor.qti.hardware.display.allocator@1.0.vendor \
-    vendor.qti.hardware.display.mapper@1.0.vendor
+    vendor.qti.hardware.display.mapper@3.0.vendor
 
 # Doze
 PRODUCT_PACKAGES += \

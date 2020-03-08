39872a7cc028c36c51b9babf2944a86ae4feabb4
Rick M <spookcity138@gmail.com>
2020-03-08 13:15:17 +0100

    Overlay: overlay-aosp -> overlay-evolution
    Just for consistency purposes

--- common.mk
+++ common.mk
@@ -19,11 +19,11 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-evolution
 
 # A/B
 AB_OTA_UPDATER := true

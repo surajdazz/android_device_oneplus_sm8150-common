b25412aeb32a7d5034c44d90d5e4a056a1aa290a
Rick M <spookcity138@gmail.com>
2020-05-02 17:10:03 +0200

    sm8150-common:Disable_mask_layer_hint

--- vendor_props.mk
+++ vendor_props.mk
@@ -132,6 +132,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_scaler=0 \
     vendor.display.disable_ui_3d_tonemap=1 \
     vendor.display.enable_null_display=0 \
+    vendor.display.disable_mask_layer_hint=1 \
     vendor.display.enable_optimize_refresh=1
 
 # DRM

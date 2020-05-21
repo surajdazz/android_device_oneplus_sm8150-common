ee33e88526434ac269e989d17fca7bfe578920b3
Pranav Vashi <neobuddy89@gmail.com>
2020-05-21 17:17:14 +0200

    sm8150-common: extract-files: Fix XML version to avoid build fail

--- extract-files.sh
+++ extract-files.sh
@@ -90,4 +90,16 @@ fi
 
 COMMON_BLOB_ROOT="${AOSP_ROOT}/vendor/${VENDOR}/${DEVICE_COMMON}/proprietary"
 
+#
+# Fix xml version
+#
+function fix_xml_version () {
+    sed -i \
+        's/xml version="2.0"/xml version="1.0"/' \
+        "$DEVICE_BLOB_ROOT"/"$1"
+}
+
+fix_xml_version product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml
+fix_xml_version product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml
+
 "${MY_DIR}/setup-makefiles.sh"

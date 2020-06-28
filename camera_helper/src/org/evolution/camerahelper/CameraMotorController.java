05b1e97391c91dfa458ce9637f56ffd70eefaeae
Rick M <spookcity138@gmail.com>
2020-06-28 21:53:14 +0200

    sm8150-common: Rebrand camera_helper
    org.lineageos -> org.evolution

--- camera_helper/src/org/evolution/camerahelper/CameraMotorController.java
+++ camera_helper/src/org/evolution/camerahelper/CameraMotorController.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.lineageos.camerahelper;
+package org.evolution.camerahelper;
 
 import android.os.FileUtils;
 import android.text.TextUtils;
--- camera_helper/src/org/evolution/camerahelper/FallSensorService.java
+++ camera_helper/src/org/evolution/camerahelper/FallSensorService.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.lineageos.camerahelper;
+package org.evolution.camerahelper;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
